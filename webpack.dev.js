db7ab1754331f1cc8550669a07bbef271ddaf0fd
Rania Alablani <53059339+ran6060@users.noreply.github.com>
2022-12-31 10:29:57 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -23,11 +23,9 @@ module.exports = {
             filename: "./index.html",
         }),
         new CleanWebpackPlugin({
-            // Simulate the removal of files
             dry: true,
             // Write Logs to Console
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
